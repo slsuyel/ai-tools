904abe4b047e216dc2d8734b22e5e59bbc67f787
Suyel Haque <slsuyel@gmail.com>
2024-01-04 02:33:05 +0600

    RrandomPasswordGenerator some issue

--- src/pages/Category/RandomGenerators/RrandomPasswordGenerator/RrandomPasswordGenerator.jsx
+++ src/pages/Category/RandomGenerators/RrandomPasswordGenerator/RrandomPasswordGenerator.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import '../RandomGenerate.css'
+import '../RandomGenerate.css';
+
 const RrandomPasswordGenerator = () => {
     const [password, setPassword] = useState('WetdiS9m4i56');
     const [quality, setQuality] = useState('Strong');
@@ -12,6 +13,62 @@ const RrandomPasswordGenerator = () => {
     });
     const passQuality = ["Very Weak", "Weak", "Good", "Strong", "Very Strong"];
 
+    const getSelectedCharSet = () => {
+        let charset = '';
+        if (characterSets.uppercase) charset += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        if (characterSets.lowercase) charset += 'abcdefghijklmnopqrstuvwxyz';
+        if (characterSets.numbers) charset += '0123456789';
+        if (characterSets.specialCharacters) charset += '!@#$%^&*()-=_+[]{}|;:,.<>?/';
+
+        // If all checkboxes are unchecked, use all available characters
+        if (!characterSets.uppercase && !characterSets.lowercase && !characterSets.numbers && !characterSets.specialCharacters) {
+            charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-=_+[]{}|;:,.<>?/';
+        }
+
+        return charset;
+    };
+
+    const generatePassword = () => {
+        const charset = getSelectedCharSet();
+        let newPassword = '';
+
+        for (let i = 0; i < passwordLength; i++) {
+            const randomChar = charset[Math.floor(Math.random() * charset.length)];
+            newPassword += randomChar;
+        }
+
+        setPassword(newPassword);
+        updateQuality(newPassword);
+    };
+
+
+
+
+
+    const updateQuality = (newPassword) => {
+        const lengthQuality = newPassword.length >= 12 ? 4 : newPassword.length >= 8 ? 3 : 2;
+        setQuality(passQuality[lengthQuality]);
+    };
+
+
+    const copyToClipboard = () => {
+        navigator.clipboard.writeText(password);
+        alert('Password Copied')
+    };
+
+    const handlePasswordLengthChange = (event) => {
+        const length = parseInt(event.target.value, 10);
+        setPasswordLength(length);
+        generatePassword();
+    };
+
+    const handleCharacterSetChange = (set, isChecked) => {
+        setCharacterSets((prevCharacterSets) => ({
+            ...prevCharacterSets,
+            [set]: isChecked,
+        }));
+        generatePassword();
+    };
 
 
 
@@ -21,49 +78,54 @@ const RrandomPasswordGenerator = () => {
             <div className='row w-100 my-1 mx-auto'>
                 <div className='col-md-8 my-1 '>
                     <div className='position-relative'>
-
-                        <input className='form-control password-view-box' defaultValue={password} type="text" disabled />
-
+                        <input className='form-control password-view-box' value={password} type="text" disabled />
                         <div className='label-reload'>
                             <label className='mb-0' htmlFor="">{quality}</label>
-
-                            <i className="fa-rotate-right fa-solid fs-3 me-2"></i></div>
-
+                            <i className="fa-rotate-right fa-solid fs-3 me-2" onClick={generatePassword}></i>
+                        </div>
                     </div>
                 </div>
-
-                <div className=' my-1 align-items-center col-md-4 d-flex justify-content-center'>
-                    <button className='submit-btn'><i className="me-2 fa-solid fa-copy"></i>Copy</button>
+                <div className='my-1 align-items-center col-md-4 d-flex justify-content-center'>
+                    <button className='submit-btn' onClick={copyToClipboard}><i className="me-2 fa-solid fa-copy"></i>Copy</button>
                 </div>
-
             </div>
 
             <div className='row w-100 my-1 mx-auto fs-5 '>
-                <div className='col-md-3 text-center'>  <label htmlFor="customRange1" className="form-label m-0 my-1 text-white">
-                    Password length:{passwordLength}
-                </label></div>
-
+                <div className='col-md-3 text-center'>
+                    <label htmlFor="customRange1" className="form-label m-0 my-1 text-white">
+                        Password length: {passwordLength}
+                    </label>
+                </div>
                 <div className='col-md-9'>
-                    <input type="range" className="form-range  my-1" id="customRange1" />
+                    <input
+                        type="range"
+                        className="form-range  my-1"
+                        id="customRange1"
+                        value={passwordLength}
+                        min="4"
+                        max="50"
+                        onChange={handlePasswordLengthChange}
+                    />
                 </div>
             </div>
 
             <div className='row w-100 my-1 mx-auto fs-5 '>
-                <div className='col-md-3 text-center'>  <label htmlFor="customRange1" className="form-label m-0 my-1 text-white">
-                    Characters used:
-                </label>
+                <div className='col-md-3 text-center'>
+                    <label htmlFor="customRange1" className="form-label m-0 my-1 text-white">
+                        Characters used:
+                    </label>
                 </div>
-
                 <div className="col-md-9 d-flex flex-wrap fs-5 justify-content-evenly text-white">
                     <div className="form-check">
                         <input
                             className="form-check-input"
                             type="checkbox"
-                            defaultValue=""
-                            id="flexCheckChecked"
-                            defaultChecked=""
+                            id="uppercaseCheckbox"
+                            checked={characterSets.uppercase}
+                            onChange={(e) => handleCharacterSetChange('uppercase', e.target.checked)}
                         />
-                        <label className="form-check-label" htmlFor="flexCheckChecked">
+
+                        <label className="form-check-label" htmlFor="uppercaseCheckbox">
                             ABC
                         </label>
                     </div>
@@ -71,11 +133,11 @@ const RrandomPasswordGenerator = () => {
                         <input
                             className="form-check-input"
                             type="checkbox"
-                            defaultValue=""
-                            id="flexCheckChecked"
-                            defaultChecked=""
+                            id="lowercaseCheckbox"
+                            checked={characterSets.lowercase}
+                            onChange={(e) => handleCharacterSetChange('lowercase', e.target.checked)}
                         />
-                        <label className="form-check-label" htmlFor="flexCheckChecked">
+                        <label className="form-check-label" htmlFor="lowercaseCheckbox">
                             abc
                         </label>
                     </div>
@@ -83,11 +145,11 @@ const RrandomPasswordGenerator = () => {
                         <input
                             className="form-check-input"
                             type="checkbox"
-                            defaultValue=""
-                            id="flexCheckChecked"
-                            defaultChecked=""
+                            id="numbersCheckbox"
+                            checked={characterSets.numbers}
+                            onChange={(e) => handleCharacterSetChange('numbers', e.target.checked)}
                         />
-                        <label className="form-check-label" htmlFor="flexCheckChecked">
+                        <label className="form-check-label" htmlFor="numbersCheckbox">
                             123
                         </label>
                     </div>
@@ -95,21 +157,16 @@ const RrandomPasswordGenerator = () => {
                         <input
                             className="form-check-input"
                             type="checkbox"
-                            defaultValue=""
-                            id="flexCheckChecked"
-                            defaultChecked=""
+                            id="specialCharactersCheckbox"
+                            checked={characterSets.specialCharacters}
+                            onChange={(e) => handleCharacterSetChange('specialCharacters', e.target.checked)}
                         />
-                        <label className="form-check-label" htmlFor="flexCheckChecked">
-                            #$&
+                        <label className="form-check-label" htmlFor="specialCharactersCheckbox">
+                            #$&amp;
                         </label>
                     </div>
-
                 </div>
-
-
             </div>
-
-
         </div>
     );
 };

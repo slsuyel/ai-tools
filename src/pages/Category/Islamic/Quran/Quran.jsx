5ae9c19485f565df627a0452bc7b4d3665e67ce5
Suyel Haque <slsuyel@gmail.com>
2024-01-16 02:43:01 +0600

    Quran

--- src/pages/Category/Islamic/Quran/Quran.jsx
+++ src/pages/Category/Islamic/Quran/Quran.jsx
@@ -1,30 +1,57 @@
 import React, { useEffect, useState } from 'react';
 import '../Islamic.css';
 import Loader from '../../../../components/Loader';
+import Select from 'react-select';
 
 const Quran = () => {
     const [sura, setSura] = useState({});
+    const [suraList, setSuraList] = useState([]);
     const [value, setValue] = useState(1);
     const [loader, setLoader] = useState(false);
     const [ayahStates, setAyahStates] = useState([]);
+    const [selectedOption, setSelectedOption] = useState(null);
+
+
+    const fetchSurahList = async () => {
+        try {
+            setLoader(true);
+            const response = await fetch('https://api.alquran.cloud/v1/surah');
+            const data = await response.json();
+            setSuraList(data.data);
+            setLoader(false);
+        } catch (error) {
+            console.error('Error fetching Surah list:', error);
+            setLoader(false);
+        }
+    };
+
+    const fetchSurahData = async () => {
+        try {
+            setLoader(true);
+            const response = await fetch(`https://api.alquran.cloud/v1/surah/${value}/ar.alafasy`);
+            const data = await response.json();
+            setSura(data.data);
+            setAyahStates(new Array(data.data.ayahs.length).fill(false));
+            setLoader(false);
+        } catch (error) {
+            console.error('Error fetching Surah data:', error);
+            setLoader(false);
+        }
+    };
 
     useEffect(() => {
-        setLoader(true);
-        const fetchData = async () => {
-            try {
-                const response = await fetch(`https://api.alquran.cloud/v1/surah/${value}/ar.alafasy`);
-                const data = await response.json();
-                setSura(data.data);
-                setAyahStates(new Array(data.data.ayahs.length).fill(false));
-                setLoader(false);
-            } catch (error) {
-                console.error('Error fetching data:', error);
-                setLoader(false);
-            }
-        };
-
-        fetchData();
+        fetchSurahData();
+        fetchSurahList()
     }, [value]);
+
+    useEffect(() => {
+        if (selectedOption !== null) {
+            setValue(selectedOption.value);
+            fetchSurahData();
+        }
+    }, []);
+
+
 
     const handlePlayPause = (ayahIndex, action) => {
         const audio = document.getElementById(`music-${ayahIndex}`);
@@ -46,26 +73,54 @@ const Quran = () => {
         setAyahStates(updatedAyahStates);
     };
 
+    const handleChange = (selectedOption) => {
+        setSelectedOption(selectedOption);
+    };
 
 
+    const handleSubmit = () => {
+        if (selectedOption) {
+            const selectedValue = selectedOption.value;
+            setValue(selectedValue);
+        }
+    };
 
     return (
         <div className=''>
             <div className='bg-gradient p-3 rounded text-white w-100'>
                 <div className='row my-2 '>
-                    <div className='col-md-8 my-auto d-flex justify-content-between px-3 text-capitalize'>
+                    <div className='col-md-4 d-flex gap-3 gap-lg-5 justify-content-center my-auto text-capitalize'>
                         <h5 className='sura-details'>{sura?.number}. {sura?.englishName}</h5>
-                        <h5 className='sura-details'>{sura?.englishNameTranslation}</h5>
                         <h5 className='sura-details'>{sura?.name}</h5>
                     </div>
+
                     <div className='col-md-4 d-flex gap-4 justify-content-end btn-next-pre'>
                         <button
                             disabled={loader}
-                            onClick={() => setValue(value - 1)} className='btn btn-danger'>Previous</button>
+                            onClick={() => setValue((prevValue) => Math.min(Math.max(prevValue - 1, 1), 114))}
+
+                            className='btn btn-danger btn-sm'>Previous</button>
                         <button
                             disabled={loader}
-                            onClick={() => setValue(value + 1)} className='btn btn-success'>Next</button>
+                            onClick={() => setValue((prevValue) => Math.min(Math.max(prevValue + 1, 1), 114))}
+
+
+                            className='btn btn-success btn-sm'>Next</button>
                     </div>
+
+                    <div className='col-md-4 d-flex gap-2 justify-content-center'>
+                        <Select
+                            className='text-dark'
+                            value={selectedOption}
+                            onChange={handleChange}
+                            options={suraList.map(s => ({ value: s.number, label: `${s.englishName}-${s.number}` }))}
+                            isSearchable={true}
+                            placeholder="Select a Sura"
+                        />
+                        <button className='bg-white border-0 rounded-1' onClick={handleSubmit}>Submit</button>
+                    </div>
+
+
                 </div>
                 <hr />
 
